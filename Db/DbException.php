61dfc7df5fcc7a30705556085d552050f6641239
Daro <daronetster@gmail.com>
2015-02-04 20:59:32 +0100

    Porządek z exceptionami, rozszerzenie Skinny\Exception::throwIf()

--- Db/DbException.php
+++ Db/DbException.php
@@ -7,7 +7,7 @@ namespace Skinny\Db;
  *
  * @author Daro
  */
-class Exception extends \Exception {
+class DbException extends \Skinny\Exception {
     // TODO: wszystkie wyjątki PDO mają być łapane i przekazywane jako $previous do tej klasy wraz z zapytaniem SQL, który spowodował wyjątek.
     // A także wszystkie wyjątki generowane przez Skinny\Db powinny być instacją tej klasy (także z odpowiadającym SQL).
 }
--- Exception.php
+++ Exception.php
@@ -3,18 +3,30 @@
 namespace Skinny;
 
 /**
- * Description of SkinnyException
+ * Standard Skinny exception class.
+ * It is base class for all other Skinny exceptions.
  *
  * @author Daro
  */
 class Exception extends \Exception {
 
-    public static function raise($exception) {
-        throw $exception;
-    }
-
-    public static function raiseIf($exception, $condition) {
+    /**
+     * Throws exception if condition is fulfilled.
+     * If exception is not provided it will be created new instance of Skinny standard exception class.
+     * @param boolean $condition condition to check
+     * @param string|\Exception $exception instance of PHP Exception class or text message for new one
+     * @throws \Exception
+     */
+    public static function throwIf($condition, $exception = null) {
         if ($condition) {
+            if (is_string($exception)) {
+                $exception = new self($exception);
+            }
+
+            if (!($exception instanceof \Exception)) {
+                $exception = new self();
+            }
+
             throw $exception;
         }
     }
