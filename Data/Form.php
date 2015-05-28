881593706b2af2dca7dd50c88ca567e23f1c4a33
Wodzu <mbadacz@confido.pl>
2015-05-28 15:04:04 +0200

    Form i Validate - rozwój

--- Data/Form.php
+++ Data/Form.php
@@ -20,12 +20,6 @@ class Form extends Validate {
     protected $_type = null;
 
     /**
-     * Label dla bieżącego pola formularza
-     * @var string
-     */
-    protected $_label = null;
-
-    /**
      * Przechowuje wartość pola
      * @var string
      */
@@ -75,12 +69,12 @@ class Form extends Validate {
                 return $this->_type;
             }
         } else {
-            if ($this->name) {
+            if ($this->_name) {
                 // Jeżeli ma nazwę to znaczy że mamy do czynienia z polem formularza
-                $controlPath = \Skinny\Path::combine(self::$_config->templatesPath, 'control', $type . '.tpl');
+                $controlPath = realpath(\Skinny\Path::combine(self::$_config->templatesPath, 'control', $type . '.tpl'));
             } else {
                 // W przypadku braku nazwy mamy do czynienia z obiektem formularza - więc szablonem
-                $controlPath = \Skinny\Path::combine(self::$_config->templatesPath, $type . '.tpl');
+                $controlPath = realpath(\Skinny\Path::combine(self::$_config->templatesPath, $type . '.tpl'));
             }
 
             if (!file_exists($controlPath)) {
@@ -109,70 +103,8 @@ class Form extends Validate {
     }
 
     /**
-     * Ustawia labelkę dla bieżącego pola lub zwraca ustawioną wartość jeśli nie podano argumentu ($label = null)
-     * 
-     * @param string $label
-     * @throws Form\Exception
-     */
-    public function label($label = null) {
-        if ($label === null) {
-            if (empty($this->_label)) {
-                // Zwraca pusty string jeżeli wartośćnie została ustawiona
-                return '';
-            } else {
-                // Zwraca ustawioną wartość
-                return $this->_label;
-            }
-        }
-
-        if (empty($label) || !is_string($label)) {
-            throw new Form\Exception("Invalid label type");
-        }
-
-        $this->_label = $label;
-
-        return $this;
-    }
-
-//    /**
-//     * Pobranie wartości pola jeżeli brak argumentów lub jej ustawienie
-//     * 
-//     * @param mixed $value
-//     * @return mixed
-//     */
-//    public function value($value = null) {
-//        if ($value === null) {
-//            $val = parent::value();
-//            if ($this->name) {
-//                $val = @$val[$this->name];
-//            }
-//            return $val;
-//        } else {
-//            // Ustawienie wartości dla pola formularza
-//            if ($this->name) {
-////                $th = $this;
-////                $data = [
-////                    $this->name => $value
-////                ];
-////                while ($th->hasParent()) {
-////                    $th = $th->getParent();
-////                    if ($th->name) {
-////                        $data = [
-////                            $th->name => $data
-////                        ];
-////                    }
-////                }
-////                $th->data = array_merge_recursive((array)$th->data, (array)$data);
-//                $this->data[$this->name] = $value;
-//            }
-//        }
-//
-//        return $this;
-//    }
-
-    /**
      * Ustawia wartość dla wybranego atrybutu (nadpisuje poprzednią)
-
+     * 
      * @param string $key klucz atrybutu
      * @param mixed $value
      * @return \Skinny\Data\Form
@@ -181,6 +113,19 @@ class Form extends Validate {
         $this->_attributes[$key] = $value;
         return $this;
     }
+    
+    /**
+     * Ustawia wiele atrybutów nadpisując ustawione wartości
+     * 
+     * @param array $attributes
+     * @return \Skinny\Data\Form
+     */
+    public function setAttributes(array $attributes) {
+        $this->_attributes = array_merge($this->_attributes, $attributes);
+        return $this;
+    }
+    
+//    public function setAttributes
 
     /**
      * Pobiera wartość wybranego atrybutu
@@ -197,7 +142,7 @@ class Form extends Validate {
      */
     public function __call($name, $arguments) {
         if ($name === 'class') {
-            return call_user_method_array('cls', $this, $arguments);
+            return call_user_method_array('__cls', $this, $arguments);
         } else {
             throw new Form\Exception("No method \"$name\"");
         }
@@ -209,7 +154,7 @@ class Form extends Validate {
      * @param string $class
      * @return string|\Skinny\Data\Form
      */
-    public function cls($class = null) {
+    private function __cls($class = null) {
         if ($class === null) {
             if (!($cls = $this->getAttribute('class'))) {
                 $cls = '';
@@ -229,4 +174,13 @@ class Form extends Validate {
         return $this->add($validator, $errorMsg, $options);
     }
 
+//    public function getControlPath() {
+//        if($this->name) {
+//            // pole formularza
+//            
+//        } else {
+//            // formularz
+//            $controlPath = \Skinny\Path::combine(self::$_config->templatesPath, 'control', $type . '.tpl');
+//        }
+//    }
 }
