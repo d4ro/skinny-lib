bfd4d6a539e9d094a9c210f5edd9fdeb5cb95abb
bonz <psmulkowski@confido.pl>
2015-07-02 14:44:11 +0200

    dodanie bibliotek walidatorów

--- Data/Validator/IsIp.php
+++ Data/Validator/IsIp.php
@@ -3,23 +3,24 @@
 namespace Skinny\Data\Validator;
 
 /**
- * Walidator sprawdzający czy wartość jest emailem na podatawie metody filter_var
+ * Walidator sprawdzający czy wartość jest poprawnym adresem IP
  */
-class IsEmail extends ValidatorBase {
+class IsIp extends ValidatorBase {
 
-    const MSG_NOT_EMAIL = 'notEmail';
+    const MSG_NOT_IP = 'notIp';
 
     public function __construct($options = null) {
         parent::__construct($options);
 
         $this->_setMessagesTemplates([
-            self::MSG_NOT_EMAIL => "Niepoprawny adres email"
+            self::MSG_NOT_IP => "Niepoprawny adres IP"
         ]);
     }
 
     public function isValid($value) {
-        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            $this->error(self::MSG_NOT_EMAIL);
+        $pattern = '/^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/';
+        if (!preg_match($pattern,$value)) {
+            $this->error(self::MSG_NOT_IP);
             return false;
         }
         return true;
