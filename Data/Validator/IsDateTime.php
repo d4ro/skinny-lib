c96aebeb8a1b1e0f369f7f059a5347567c7781a7
Azcimas <adr.szymanski@gmail.com>
2017-12-29 17:05:54 +0100

    poprawki w walidatorze datetime

--- Data/Validator/IsDateTime.php
+++ Data/Validator/IsDateTime.php
@@ -41,8 +41,23 @@ class IsDateTime extends IsString {
          * DO POPRAWIENIA
          * TODO
          */
+        $pattern = '/^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/';
+
+        if (!preg_match($pattern, $value)) {
+            $this->error(self::MSG_NOT_DATETIME);
+            return false;
+        }
+
         $datetimeExplode = explode(" ", $value);
-        $dateExplode     = explode("-", $datetimeExplode[0]);
+        if (count($datetimeExplode) != 2) {
+            $this->error(self::MSG_NOT_DATETIME);
+            return false;
+        }
+        $dateExplode = explode("-", $datetimeExplode[0]);
+        if (count($dateExplode) != 3) {
+            $this->error(self::MSG_NOT_DATETIME);
+            return false;
+        }
         if (!checkdate($dateExplode[1], $dateExplode[2], $dateExplode[0])) {
             $this->error(self::MSG_NOT_DATETIME);
             return false;
